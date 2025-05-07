4c08fa92914e3711c124224e25dff01fca8ceffc
MR-WoLf-ux <zakersamuraee3@gmail.com>
2025-05-07 17:45:32 +0330

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'my-new-pwa-cache-v1';
+const urlsToCache = [
+    'index.html',
+    'style.css',
+    'script.js',
+    'https://fonts.googleapis.com/css2?family=Vazirmatn:wght@400;700&display=swap',    
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+            .catch(err => console.error('Install Error:', err))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .catch(() => {
+                        return new Response(
+                            '<h1 style="text-align: center; font-family: Arial;">شما آفلاین هستید</h1>' +
+                            '<p style="text-align: center; color: #666;">لطفاً اتصال اینترنت خود را بررسی کنید.</p>',
+                            {
+                                headers: { 'Content-Type': 'text/html; charset=utf-8' }
+                            }
+                        );
+                    });
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
